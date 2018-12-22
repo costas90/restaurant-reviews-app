27887c0c88aa98532ecfa562153e55380e17e57b
constantinos <constantinos.n.cn@gmail.com>
2018-12-22 17:29:17 +0200

    Add a service worker

---
+++ sw.js
@@ -0,0 +1,80 @@
+var CACHE_NAME = 'restaurant-reviews-app-v1';
+var urlsToCache = [
+    '/',
+    '/restaurant.html',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/js/main.js',
+    '/js/dbhelper.js',
+    '/js/restaurant_info.js'
+];
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+}
+
+// Listen for install event and create a cache
+// if none exists
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Listen for the activate event
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['restaurant-reviews-app-v1'];
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+});
+
+// Return cached responses on fetch event
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+});
+
