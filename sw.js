ba7233ff48b4c7a255ec4c5c7b82beba54c3303c
constantinos <constantinos.n.cn@gmail.com>
2018-12-22 19:14:08 +0200

    Fix accessibility and SW issues

--- sw.js
+++ sw.js
@@ -19,19 +19,6 @@ var urlsToCache = [
     '/js/restaurant_info.js'
 ];
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-}
-
 // Listen for install event and create a cache
 // if none exists
 self.addEventListener('install', function(event) {
